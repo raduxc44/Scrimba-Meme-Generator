21f5b3a5e34549403ea5acdb48f906e99ca8e99d
raduxc44 <raduxc44@gmail.com>
2022-07-21 18:29:20 +0300

    Added the form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import Meme from './components/Meme';
 
 
 function App() {
   return (
-    <Header />
+    <div className='App'>
+      <Header />
+      <Meme />
+    </div>
   );
 }
 
---
+++ src/components/Meme.jsx
@@ -0,0 +1,15 @@
+function Meme () {
+    return(
+        <div className="Meme">
+            <form action="">
+                <div className="user-input">
+                    <input type="text" placeholder="Shut up"/>
+                    <input type="text" placeholder="and take my money"/>
+                </div>
+                <input className="button" type="button" value="Get a new meme image  🖼" />
+            </form>
+        </div>
+    ) 
+}
+
+export default Meme
